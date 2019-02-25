bef71f3c1988cac76591f795ac9d706d182001a9
AlejandroCallara <alejandrocallara@gmail.com>
2019-02-25 16:54:27 +0100

    Update of SmRg_vis3D
    added functionality: 
    
    superimpose swc file on segmented structure.

--- SmRG/functions/SmRG_vis3D.m
+++ SmRG/functions/SmRG_vis3D.m
@@ -1,14 +1,16 @@
-function SmRG_vis3D(J)
+function SmRG_vis3D(J,tree)
 % SmRG_vis3D: 
-%           3D plot of binary 3D dataset. (ideally the output of
-%           SmRG_regionGrowing
+%           3D plot of binary 3D dataset (ideally the output of SmRG_regionGrowing)
+%           with optional swc superimposed. 
+%           .
 %
 % Syntax:
-%           SmRG_getBackground(J)
+%           SmRG_vis3D(J)
+%           SmRG_vis3D(J,tree)
 %
 % Inputs:
-%           Vin: 2D image 
-%           method: one of {'otsu','mm'}
+%           J: 3D binary dataset 
+%           tree = swc Nx7 matrix
 % Outputs:
 %           background:  mean value of background pixels
 %           Vb: image with only background pixels
@@ -18,11 +20,30 @@ if nargin <1
     help SmRG_vis3D
     return
 end
-
-[p,v] = isosurface(J, 0.5);
 figure;
+if nargin ==2
+    color = [1 0 0];
+    hold on
+    for i = 1 : size(tree, 1)
+        % Draw a line between the current node and its parent
+        cnode = tree(i, 3:5);
+        parent = tree(i, 7);
+        [pidx] = find(tree(:, 1) == parent);
+        if numel(pidx) == 1
+            pnode = tree(pidx, 3:5);
+            h = plot3([cnode(1);pnode(1)], [cnode(2);pnode(2)], [cnode(3); pnode(3)],...
+                'Color', color);
+            set(h(1),'linewidth',3);
+        end
+    end
+    %The following line of code can adjust axis ratio
+    daspect([1 1 1])
+    alpha 0
+end
+[p,v] = isosurface(J, 0.5);
+
 patch('Faces',p, 'Vertices',v,'FaceColor','green',...
-      'EdgeColor','none','FaceAlpha',1);
+      'EdgeColor','none','FaceAlpha',0.25);
 axis equal;
 
 camlight; 
